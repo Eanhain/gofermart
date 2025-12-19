3fd1217c176d7cb9c35423a6a6d7ec68e16c4efd
Pavel Kirillov <pkirillovs@ozon.ru>
2025-12-19 20:41:56 +0300

    del unused interface method

--- internal/domain/service.go
+++ internal/domain/service.go
@@ -10,7 +10,6 @@ type Service interface {
 	AuthUser(ctx context.Context, user dto.UserInput) (bool, error)
 	RegUser(ctx context.Context, user dto.UserInput) error
 	GetUserWithdrawals(ctx context.Context, user dto.User) error
-	PostUserWithdraw(ctx context.Context, user dto.User) error
 	PostUserOrder(ctx context.Context, username string, order string) error
 	GetUserOrders(ctx context.Context, username string) (dto.OrdersDesc, error)
 	GetUserBalance(ctx context.Context, username string) (dto.Amount, error)
